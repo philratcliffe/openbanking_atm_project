297503e1978d450776747bfe4b0adc41b5a319be
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-02-06 16:19:18 +0000

    Create separate map and list view.

--- atms/views.py
+++ atms/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 import requests
 import json
 
-def list(request):
+def map(request):
     """
-    Retuns ATM info
+    Displays ATM info on a map
     """
 
     r = requests.get('https://api.lloydsbank.com/open-banking/v1.2/atms')
 
-    # walk through the ATM objects and extract lat and long to dict
+    # walk through the ATM objects and extract latitude and longitude
     data = r.json()['data']
     locations = []
     for atm in data:
@@ -18,4 +18,15 @@ def list(request):
         locations.append((latitude, longitude))
 
 
-    return render(request, 'index.html', {'locations':locations})
+    return render(request, 'map.html', {'locations':locations})
+
+def list(request):
+    """
+    Returns ATM info
+    """
+
+    r = requests.get('https://api.lloydsbank.com/open-banking/v1.2/atms')
+    atm_json_data = r.json()
+    atm_json_pretty = json.dumps(r.json(), sort_keys=True, indent=4)
+
+    return render(request, 'list.html', {'atm_json_pretty':atm_json_pretty})
