274c8bb9287e072e194b59c9d64a8ad9f5a223b5
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-10-21 18:41:47 +0100

    Cleared some PEP8 errors.

--- atms/models.py
+++ atms/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
+
 # An ATM can have many ATMServices
 class ATM(models.Model):
     atmid = models.CharField(max_length=32, unique=True)
 
     def __str__(self):
-        return(self.atmid)
+        return self.atmid
 
 
 
