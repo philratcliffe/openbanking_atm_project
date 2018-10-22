693003e2e76f4257df807ba462645990497cdfc7
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-10-22 17:57:41 +0100

    Use a manager for deleting objects.

--- atms/management/commands/populateDB.py
+++ atms/management/commands/populateDB.py
@@ -6,14 +6,13 @@ from atms.models import ATM
 class Command(BaseCommand):
     help = 'Reads data from the ATM API and populates the db'
 
-
     def handle(self, *args, **options):
         r = requests.get('https://api.lloydsbank.com/open-banking/v2.2/atms')
         data = r.json()['data']
         atm_list = data[0]['Brand'][0]['ATM']
 
-        # Delete existing entries in the database
-        ATM.objects.all().delete()
+        # First, delete any existing entries in the database
+        ATM.objects.delete_all()
 
         for atm_data in atm_list:
             atm = ATM()
--- atms/models.py
+++ atms/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
+class ATMManager(models.Manager):
+
+    def delete_all(self):
+        return self.all().delete()
+
+
 # An ATM can have many ATMServices
 class ATM(models.Model):
     atmid = models.CharField(max_length=32, unique=True)
+
+    objects = ATMManager()
 
     def __str__(self):
         return self.atmid
