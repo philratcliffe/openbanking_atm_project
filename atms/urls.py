3a3bb8b33ee9f2e2c9283763f83d1a57da5c36fa
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-10-22 12:56:01 +0100

    Support a home page with links to the app's views.

--- atms/urls.py
+++ atms/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path('list', views.list, name='list'),
-    path('map', views.map, name='map'),
+    path('', views.list, name='atms-views-list'),
+    path('list/', views.list, name='atms-views-list'),
+    path('map/', views.map, name='atms-views-map'),
 ]
