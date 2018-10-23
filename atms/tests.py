70cc06c8b7c69ab509f8a43631cf2dcdc2149a01
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-10-23 09:23:29 +0100

    Add an initial test.

--- atms/tests.py
+++ atms/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from .models import ATM
 
-# Create your tests here.
+
+class ATMTestCase(TestCase):
+    def setUp(self):
+        ATM.objects.create(atmid='1')
+
+    def test_get_object_from_id(self):
+        atm = ATM.objects.get(atmid='1')
+        self.assertTrue(atm, "There is no atm object created.")
+
+
+
+
